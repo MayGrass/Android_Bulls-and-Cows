97a1175848b7c55a32fe2c9dc2608f8239d83d99
DCH <dching0121@gmail.com>
2019-11-11 13:45:55 +0800

    add how to checkAB

--- app/src/main/java/tw/org/iii/iiiandroid04/MainActivity.java
+++ app/src/main/java/tw/org/iii/iiiandroid04/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -12,11 +14,17 @@ import java.util.HashSet;
 import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String answer;
+    private int dig = 3;
+    private EditText input;
+    private TextView log;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        input = findViewById(R.id.input);
+        log = findViewById(R.id.log);
+        answer = createAnswer(3);
     }
 
     //產生答案
@@ -34,15 +42,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void guess(View view) {
+        String strInput= input.getText().toString();
+        String result = checkAB(strInput);
+        log.append(strInput + "=>" + result + "\n");
+        input.setText("");
     }
 
     public void newGame(View view) {
-        createAnswer(4);
     }
 
     public void setting(View view) {
     }
 
     public void exit(View view) {
+    }
+
+    private String checkAB(String guess) {
+        int a, b; a = b = 0;
+        for (int i=0; i<guess.length(); i++) {
+            if (guess.charAt(i) == answer.charAt(i)) //字串比對第i碼對答案第i碼
+                a++;
+            else if (answer.indexOf(guess.charAt(i)) != -1) //indexOf只有在字串中沒出現過就會回傳-1
+                b++;
+        }
+        return a + "A" + b + "B";
     }
 }
